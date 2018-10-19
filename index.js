ae54d79ab3aba701ff8d1703b11e03781b07f7e3
Shaba9 <shaba92@gmail.com>
2018-10-19 13:20:36 -0700

    clock updates

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var today = new Date();
-var hour = today.getHours();
-var minute = today.getMinutes();
-var second = today.getSeconds();
- 
-if(minute < 10) {
-   minute = '0' + minute;
-};
- 
-if(second < 10){
-  second = '0' + second;
-};
- 
-var time = hour + ':' + minute + ':' + second;
- 
-document.write("<h1>" + time + "</h1>");
+function Time() {
+  var today = new Date();
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  var form = 'AM';
+
+  if(hour > 12) {
+    hour -= 12;
+    form = 'PM';
+  }
+   
+  if(minute < 10) {
+     minute = '0' + minute;
+  };
+   
+  if(second < 10){
+    second = '0' + second;
+  };
+   
+  var time = hour + ':' + minute + ':' + second + ' ' + form;
+   
+  document.getElementById('realTime').innerHTML = time;
+  setTimeout(Time, 1000);
+}
+
+Time();
