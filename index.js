11205088b287c72fd27652665659748719ce2216
Shaba9 <shaba92@gmail.com>
2018-10-20 18:48:52 -0700

    adding new time form

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ function Time() {
   var hour = today.getHours();
   var minute = today.getMinutes();
   var second = today.getSeconds();
-  var form = 'AM';
+  var period = 'AM';
+  var time = '0:00:00';
 
-  if(hour > 12) {
-    hour -= 12;
-    form = 'PM';
+  if(hour >= 12) {
+    if(hour > 12){
+      hour -= 12;
+    }
+    period = 'PM';
   }
    
   if(minute < 10) {
@@ -18,10 +21,31 @@ function Time() {
     second = '0' + second;
   };
    
-  var time = hour + ':' + minute + ':' + second + ' ' + form;
+  time = hour + ':' + minute + ':' + second + ' ' + period;
    
   document.getElementById('realTime').innerHTML = time;
   setTimeout(Time, 1000);
 }
 
 Time();
+
+function SetTime(event){
+  var el = event.target.elements;
+  var newHour = el.newHour.value;
+  var newMinute = el.newMinute.value;
+  var newSecond = el.newSecond.value;
+  var newPeriod = el.newPeriod.value;
+  var newTime;
+  
+  if(newMinute < 10) {
+    newMinute = '0' + newMinute;
+  };
+    
+  if(newSecond < 10){
+    newSecond = '0' + newSecond;
+  };
+
+  newTime = newHour + ':' + newMinute + ':' + newSecond + ' ' + newPeriod;
+
+  document.getElementById('realTime').innerHTML = newTime;
+}
